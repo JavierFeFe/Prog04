8d07fda20903c0ed8268c60809c29425ac1cb870
JavierFeFe <44543081+JavierFeFe@users.noreply.github.com>
2018-11-13 20:51:13 +0100

    Update primos.java

--- ejercicioTarea04/primos.java
+++ ejercicioTarea04/primos.java
@@ -15,24 +15,26 @@ import java.util.Scanner;
  * @author Javier Fernández Ferrol
  */
 public class Supuesto3 {
-    private static final List<Integer> primos = new ArrayList<>();
-    private static List<Integer> resultado;
-    private static int numero;
+    private static final List<Long> primos = new ArrayList<>();
+    private static List<Long> resultado;
+    private static long numero;
+    private static final byte longitud = 12; //A partir de 12 el tiempo se hace demasiado largo
     public static void main (String[] args){
-        primos.addAll(Arrays.asList(2, 3, 5)); //Primos básicos para empezar       
+        primos.addAll(Arrays.asList(2l, 3l, 5l)); //Primos básicos para empezar       
         resultado = new ArrayList<>();
         Scanner teclado = new Scanner(System.in);
         numero = -1;
         String in="";
         while (numero == -1){
-            System.out.print("Introduce el número con un máximo de 9 dígitos: ");
+            System.out.print("Introduce el número con un máximo de " + longitud + " dígitos: ");
             in = teclado.next();
             if (!in.matches("\\d*")){
                 System.out.println("¡¡" + in + " no es un formato de número válido!!");
-            }else if (in.length() > 9){
+            }
+            else if (in.length() > longitud){
                 System.out.println("¡¡El número es demasiado largo!!");
             }else{
-                numero = Integer.parseInt(in);
+                numero = Long.parseLong(in);
             }
         }
         System.out.println("Generando nºs primos necesarios...");
@@ -45,9 +47,9 @@ public class Supuesto3 {
                 La siguiente parte es para generar el texto con exponenciales
         */               
         String texto="";
-        int numeroActivo = -1;
+        long numeroActivo = -1;
         int exponente = 0;
-        for (int num: resultado){
+        for (long num: resultado){
             if (numeroActivo == num){
                 exponente++;
             }else{
@@ -59,8 +61,8 @@ public class Supuesto3 {
         texto += generaNumero(numeroActivo, exponente);
         System.out.println(texto);
     }
-    private static void generaPrimos(int numero){
-        int contador = primos.get(primos.size() -1);
+    private static void generaPrimos(long numero){
+        long contador = primos.get(primos.size() -1);
         while (numero > Math.pow(primos.get(primos.size() -1) +2 ,2)-1){ //Siempre q numero sea mayor que el último primo + 2 al cuadrado - 1
             contador = contador + 2; //Sumando 2 evito los números pares
             String contTexto = String.valueOf(contador);
@@ -68,7 +70,7 @@ public class Supuesto3 {
                 contador = contador + 2; 
             }
             boolean esPrimo = true;
-            for (int primo: primos){
+            for (long primo: primos){
                 if (contador % primo == 0){
                     esPrimo = false;
                     //System.out.println(contador + " es divisible entre: " + primo);
@@ -81,21 +83,21 @@ public class Supuesto3 {
             }
         }
     }
-    private static String generaNumero(int numeroActivo, int exponente){
+    private static String generaNumero(long numeroActivo, long exponente){
         String texto = "";
         texto += numeroActivo;
         texto += (exponente>1)?("^" + exponente):"";
         return texto;
     }
-    private static int creaFactores(int numero){
-        for (int primo : primos){ 
+    private static long creaFactores(long numero){
+        for (long primo : primos){ 
             if (primo >= numero){ //Si el primo es mayor o igual que el número evitamos hacer la división y finalizamos el bucle.
                 resultado.add(numero);
                 return -1;
             }
             if ((numero % primo) == 0){ //Si el resto de la división es 0 quiere decir que es divisible.
                 resultado.add(primo);
-                int valor = numero/primo;
+                long valor = numero/primo;
                 return valor;
             }
         }
