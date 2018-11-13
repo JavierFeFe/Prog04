9e644d020c3ab0f99c94bbd5d95b30db125b34ec
JavierFeFe <44543081+JavierFeFe@users.noreply.github.com>
2018-11-13 22:23:19 +0100

    Update primos.java

--- ejercicioTarea04/primos.java
+++ ejercicioTarea04/primos.java
@@ -15,18 +15,18 @@ import java.util.Scanner;
  * @author Javier Fernández Ferrol
  */
 public class Supuesto3 {
-    private static final List<Long> primos = new ArrayList<>();
-    private static final List<Long> primosFinales = new ArrayList<>();
-    private static List<Long> resultado;
-    private static long numero;
+    private static final List<Long> primos = new ArrayList<>(); //Lista de primos ordenados numéricamente
+    private static final List<Long> primosFinales = new ArrayList<>(); //Lista de primos por lo que sabemos q es divisible nuestro número
+    private static List<Long> resultado; //Colección de factores
+    private static long numero; //El número con el que trabajaremos
     private static final byte longitud = 12; //A partir de 12 el tiempo de procesamiento se hace demasiado largo en caso de resultar ser un número primo
     public static void main (String[] args){
-        primos.addAll(Arrays.asList(2l, 3l, 5l, 7l)); //Primos básicos para empezar       
-        resultado = new ArrayList<>();
+        primos.addAll(Arrays.asList(2l, 3l, 5l, 7l)); //Primos más básicos para primeras comprobaciones     
+        resultado = new ArrayList<>(); 
         Scanner teclado = new Scanner(System.in);
         numero = -1;
         String in="";
-        while (numero == -1){
+        while (numero == -1){ //Fuerza la introducción de un número válido con una longitud máxima especificada en la variable lontitud
             System.out.print("Introduce el número con un máximo de " + longitud + " dígitos: ");
             in = teclado.next();
             if (!in.matches("\\d*")){
@@ -39,7 +39,7 @@ public class Supuesto3 {
             }
         }
         System.out.println("Generando nºs primos necesarios...");
-        generaPrimos(numero);
+        generaPrimos(numero); //Se generan los primos por los que es divisible nuestro número
         System.out.print("Factores primos: ");
         while (numero != -1){ //El valor -1 se usará como marcador para finalizar el bucle
             numero = creaFactores(numero); //uso el método división definido más abajo
@@ -59,22 +59,22 @@ public class Supuesto3 {
             }
             numeroActivo = num;
         }
-        texto += generaNumero(numeroActivo, exponente);
-        System.out.println(texto);
+        texto += generaNumero(numeroActivo, exponente); //Añado el último número del bucle
+        System.out.println(texto); //Muestro el resultado
     }
     private static void generaPrimos(long numero){
         long contador = primos.get(primos.size() -1);
         boolean basicos = true;
         while (basicos){ //Este bucle simplifica el nº lo máximo posible intentando factorizar entre 2, 3, 5 y 7 (los primos más básicos)
+            basicos = false;
             for (long primo: primos){
                 if (numero % primo == 0){
                     if (!primosFinales.contains(primo)){ //Si primosFinales no contiene el nuevo primo lo añade
                         primosFinales.add(primo);
                     }
                     numero = numero/primo;
-                    break;
+                    basicos = true;
                 }
-               basicos = false; 
             }
         }
         while (numero > Math.pow(primos.get(primos.size() -1) +2 ,2)-1){ //Siempre q numero sea mayor que el último primo + 2 al cuadrado - 1
@@ -83,24 +83,26 @@ public class Supuesto3 {
             if (contTexto.charAt(contTexto.length()-1) == '5'){ //Evito los múltiplos de 5 sin realizar operaciones matemáticas
                 contador = contador + 2; 
             }
+            /* 
+                Podría añadir más reglas mnemotécnicas para evitar operaciones con otros nºs, por ejemplo del 3 y del 7
+            */
             boolean esPrimo = true;
-            for (long primo: primos){
+            for (long primo: primos){ //Recorro el bucle para saber si es divisible entre los primos identificados
                 if (contador % primo == 0){
                     esPrimo = false;
                     break;
                 }
             }
             if (esPrimo){
-                //System.out.println(contador + " es primo!!");
                 primos.add(contador);
-                if (numero % contador == 0){
+                if (numero % contador == 0){ //Compruebo si nuestro número es divisible entre el nuevo número primo
                     numero = numero/contador; // Simplifico el nº de primos necesarios
                     primosFinales.add(contador); //Añade nuevo primo a primosFinales
                 }
             }
         }
     }
-    private static String generaNumero(long numeroActivo, long exponente){
+    private static String generaNumero(long numeroActivo, long exponente){ //Este método crea una cadena de texto que representa un número con exponencial (o sin el)
         String texto = "";
         texto += numeroActivo;
         texto += (exponente>1)?("^" + exponente):"";
@@ -112,7 +114,7 @@ public class Supuesto3 {
                 resultado.add(numero);
                 return -1;
             }
-            if ((numero % primo) == 0){ //Si el resto de la división es 0 quiere decir que es divisible.
+            if ((numero % primo) == 0){ //Si el resto de la división es 0 quiere decir que es un factor.
                 resultado.add(primo);
                 long valor = numero/primo;
                 return valor;
@@ -122,4 +124,3 @@ public class Supuesto3 {
         return -1;
     } 
 }
-
