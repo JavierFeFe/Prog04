18cc10bc3713a025b2c3104a407061c747480174
JavierFeFe <44543081+JavierFeFe@users.noreply.github.com>
2018-11-14 19:06:14 +0100

    Update primos.java

--- ejercicioTarea04/primos.java
+++ ejercicioTarea04/primos.java
@@ -15,89 +15,83 @@ import java.util.Scanner;
  * @author Javier Fernández Ferrol
  */
 public class Supuesto3 {
-    private static final List<Long> primos = new ArrayList<>(); //Lista de primos ordenados numéricamente
-    private static final List<Long> primosFinales = new ArrayList<>(); //Lista de primos por lo que sabemos q es divisible nuestro número
-    private static long numero; //El número con el que trabajaremos
-    private static final byte longitud = 12; //A partir de 12 el tiempo de procesamiento se hace demasiado largo en caso de resultar ser un número primo
+    private static final List<Long> PRIMOS = new ArrayList<>(); //Lista de primos ordenados numéricamente
+    private static final List<Long> FACTORES = new ArrayList<>(); //Lista de factores de nuestro número
+    private static long numero = -1; //El número con el que trabajaremos
+    private static final byte LONGITUD = 12; //A partir de 12 el tiempo de procesamiento se hace demasiado largo en caso de resultar ser un número primo
     public static void main (String[] args){
-        primos.addAll(Arrays.asList(2l, 3l, 5l, 7l)); //Primos más básicos para primeras comprobaciones     
+        PRIMOS.addAll(Arrays.asList(2l, 3l, 5l, 7l)); //Primos más básicos para primeras comprobaciones     
         Scanner teclado = new Scanner(System.in);
-        numero = -1;
-        String in="";
         while (numero == -1){ //Fuerza la introducción de un número válido con una longitud máxima especificada en la variable lontitud
-            System.out.print("Introduce el número con un máximo de " + longitud + " dígitos: ");
-            in = teclado.next();
-            if (!in.matches("\\d*")){
-                System.out.println("¡¡" + in + " no es un formato de número válido!!");
-            }
-            else if (in.length() > longitud){
-                System.out.println("¡¡El número es demasiado largo!!");
-            }else{
-                numero = Long.parseLong(in);
-            }
+            System.out.print("Introduce el número con un máximo de " + LONGITUD + " dígitos: ");
+            String in = teclado.next();
+            if (!in.matches("\\d+")) System.out.println("¡¡" + in + " no es un formato de número válido!!");
+            else if (in.length() > LONGITUD) System.out.println("¡¡El número es demasiado largo!!");
+            else numero = Long.parseLong(in);
         }
         System.out.println("Generando nºs primos necesarios...");
-        generaPrimos(numero); //Se generan los primos por los que es divisible nuestro número   
+        generaPrimos(numero); //Se generan los primos necesarios y se factoriza el número  
         /* 
                 La siguiente parte es para generar el texto con exponenciales
         */ 
-        if (primosFinales.size() == 1){
-            System.out.println("El número " + primosFinales.get(0) + " es primo.");
+        if (FACTORES.size() == 1){
+            System.out.println("El número " + FACTORES.get(0) + " es primo.");
         }else{
             System.out.print("Factores primos: ");
-            String texto="";
-            long numeroActivo = -1;
+            String resultado=""; //Aquí almaceno la cadena de texto con los factores y sus potencias
+            long numeroActivo = -1l;
             int exponente = 0;
-            for (long num: primosFinales){
-                if (numeroActivo == num){
-                    exponente++;
-                }else{
-                    texto += (numeroActivo > -1)?generaNumero(numeroActivo, exponente) + " * ":"";
+            for (long num: FACTORES){
+                if (numeroActivo == num) exponente++;
+                else{
+                    resultado += (numeroActivo > -1)?generaNumero(numeroActivo, exponente) + " * ":"";
                     exponente = 1;
                 }
                 numeroActivo = num;
             }
-            texto += generaNumero(numeroActivo, exponente); //Añado el último número del bucle
-            System.out.println(texto); //Muestro el resultado
+            resultado += generaNumero(numeroActivo, exponente); //Añado el último número del bucle
+            System.out.println(resultado); //Muestro el resultado
         }
     }
     private static void generaPrimos(long numero){
-        int posicionInicial= primos.size() -1;
-        long contador = primos.get(posicionInicial);
-        for (long primo: primos){//Este bucle simplifica el nº lo máximo posible intentando factorizar entre 2, 3, 5 y 7 (los primos más básicos)
+        long ultimoPrimo = PRIMOS.get(PRIMOS.size() -1);
+        for (long primo: PRIMOS){//Este bucle simplifica el nº lo máximo posible intentando factorizar entre 2, 3, 5 y 7 (los primos más básicos)
             while (numero % primo == 0){
-                primosFinales.add(primo);
-                numero = numero/primo;
+                FACTORES.add(primo);
+                numero /= primo;
             }
         }
-        while (numero > Math.pow(primos.get(primos.size() -1) +2 ,2)-1){ //Siempre q numero sea mayor que el último primo + 2 al cuadrado - 1
-            contador += 2; //Sumando 2 evito la comprobación de los divisibles entre 2
-            String contTexto = String.valueOf(contador);
-            while (contTexto.charAt(contTexto.length()-1) == '5'){ //Evito la comprobación de los divisibles entre 5
-                contador += 2; 
-                contTexto = String.valueOf(contador);
+        PRIMOS.remove(0); //Evito el 2 (regla del 2)
+        PRIMOS.remove(2); //Evito el 5 (regla del 5)
+        while (numero > Math.pow(PRIMOS.get(PRIMOS.size() -1) +2 ,2)-1){ //Siempre q numero sea mayor que el último primo + 2 (para evitar par) al cuadrado - 1 (porque no sabemos si ese número será primo)
+            //Regla del 2 (Sumando 2 evito la comprobación de los divisibles entre 2 (números pares))
+            ultimoPrimo += 2; //Aplico regla del 2
+            //Regla del 5 (Si el nº acaba en 5 es múltiplo de 5 (el 0 ya es par))
+            String contTexto = String.valueOf(ultimoPrimo);
+            while (contTexto.charAt(contTexto.length()-1) == '5'){
+                ultimoPrimo += 2; //Aplico regla del 2
+                contTexto = String.valueOf(ultimoPrimo);
             }
-            /* 
-                Podría añadir reglas mnemotécnicas para evitar comprobaciones con ciertos nºs, como el 3 o el 7
+            /*
+                Podría añadir más reglas mnemotécnicas para evitar comprobaciones con ciertos nºs, como el 3 o el 7 
+                pero tengo mis dudas de que esto mejore los tiempos de procesamiento (al menos en los rangos q estamos manejando).
             */
             boolean esPrimo = true;
-            for (long primo: primos){ //Recorro el bucle para saber si es divisible entre los primos identificados
-                if (contador % primo == 0){
+            for (long primo: PRIMOS){ //Recorro el bucle para saber si es divisible entre los primos identificados
+                if (ultimoPrimo % primo == 0){
                     esPrimo = false;
-                    break;
+                    break; //Ya sabemos que no es primo así q no es necesario continuar el bucle
                 }
             }
-            if (esPrimo){
-                primos.add(contador);
-                while (numero % contador == 0){ //Compruebo si nuestro número es divisible entre el nuevo número primo
-                    numero = numero/contador; // Simplifico el nº de primos necesarios
-                    primosFinales.add(contador); //Añade nuevo primo a primosFinales
+            if (esPrimo){ //Si el bucle anterior no encuentra divisores quiere decir que es primo
+                PRIMOS.add(ultimoPrimo);
+                while (numero % ultimoPrimo == 0){ //Compruebo si nuestro número es divisible entre el nuevo número primo
+                    numero /= ultimoPrimo; // Simplifico el nº de primos necesarios por recorrer
+                    FACTORES.add(ultimoPrimo); //Añade nuevo primo a primosFinales
                 }
             }
         }
-        if (numero != 1){ //Si el nº final es !=1 quiere decir q no se pudo factorizar por todos los nºs primos generados, por lo q debe de ser primo
-            primosFinales.add(numero);
-        }
+        if (numero != 1) FACTORES.add(numero); //Si el nº final es desigual 1 quiere decir q no se pudo factorizar por todos los nºs primos generados, por lo q debe de ser primo
     }
     private static String generaNumero(long numeroActivo, long exponente){ //Este método crea una cadena de texto que representa un número con exponencial (o sin el)
         String texto = "";
