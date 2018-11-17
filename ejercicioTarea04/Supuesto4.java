5c7d3ff6e79176185679ded021213e6c8e5fb3b5
JavierFeFe <44543081+JavierFeFe@users.noreply.github.com>
2018-11-17 12:15:52 +0100

    Create Supuesto4.java

---
+++ ejercicioTarea04/Supuesto4.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioTarea04;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Javier Fernández Ferrol
+ */
+public class Supuesto4 {
+    
+    /**
+     *
+     * @param args
+     */
+    public static void main (String[] args){
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Introduce el nombre: ");
+        String nombre = teclado.next();
+        if (!nombre.matches("[A-Za-zÁÉÍÓÚñáéíóúÑ]+")){ //Compruebo si el texto introducido es un número válido
+            System.out.println("¡¡" + nombre + " no es nombre válido!");
+            return;
+        }
+        System.out.print("Introduce la edad: ");
+        String ed = teclado.next();
+        if (!ed.matches("\\d+")){
+            System.out.println("¡¡" + ed + " no es un formato de edad válido!");
+            return;
+        }
+        short edad = Short.parseShort(ed);
+        if (edad > 130){
+            System.out.println("¡¡" + edad + " es una edad demasiado alta!");
+            return;
+        }
+        char categoria = '0';
+        if (edad ==0){
+            System.out.println("¡¡" + edad + " no es una edad válida!");
+        }else if (edad <= 25){
+            categoria = 'A';
+        }else if (edad <= 50){
+            categoria = 'B';
+        }else{
+            categoria = 'C';
+        }
+        System.out.println(nombre + " con " + edad + " años pertenecea a la categoría " + categoria);
+    
+    }
+    
+}
